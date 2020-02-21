f00f0fbf8e0812c153ccf3c48cb079894c90c7a8
Goodness Ezeokafor <gootech442@yahoo.com>
2020-02-21 14:48:15 +0100

    new changes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,7 +7,7 @@ return(
                <div class="navbar-header">
                   <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
                   <a class="navbar-brand" href="#home">
-                     <h2 style={{color:"black", fontSize:"30px"}}>CIHUB</h2>
+                     <h2 style={{color:"white", fontSize:"30px"}}>CIHUB</h2>
                   </a>
                </div>
                <div id="navbar" class="collapse navbar-collapse">
