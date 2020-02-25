c86e7f74bb34660902e3bfbaec8e543d3e510a1f
Goodness Ezeokafor <gootech442@yahoo.com>
2020-02-25 12:18:45 +0100

    gallery

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -16,52 +16,62 @@ const Gallery = () => {
 
 
             <div class={ classes.gallery_product +  " col-lg-4 col-md-4 col-sm-4 col-xs-12 filter hdpe" }>
-                <img src="http://fakeimg.pl/365x365/" class="img-responsive" />
+                <img src="/images/DSC_1426.jpg" class="img-responsive" />
             </div>
 
             <div class={classes.gallery_product + " col-lg-4 col-md-4 col-sm-4 col-xs-12 filter sprinkle"}>
-                <img src="http://fakeimg.pl/365x365/" class="img-responsive" />
+                <img src="/images/DSC_1447.jpg" class="img-responsive" />
             </div>
 
             <div class={classes.gallery_product +  " col-lg-4 col-md-4 col-sm-4 col-xs-12 filter hdpe" }>
-                <img src="http://fakeimg.pl/365x365/" class="img-responsive" />
+                <img src="/images/DSC_1532.jpg" class="img-responsive" />
             </div>
 
             <div class={classes.gallery_product + " gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-12 filter irrigation"}>
-                <img src="http://fakeimg.pl/365x365/" class="img-responsive" />
+                <img src="/images/DSC_1664.jpg" class="img-responsive" />
             </div>
 
             <div class={classes.gallery_product +  " gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-12 filter spray" }>
-                <img src="http://fakeimg.pl/365x365/" class="img-responsive" />
+                <img src="/images/DSC_1615.jpg" class="img-responsive" />
             </div>
 
             <div class={classes.gallery_product +  " gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-12 filter irrigation" }>
-                <img src="http://fakeimg.pl/365x365/" class="img-responsive" />
+                <img src="/images/DSC_1485.jpg" class="img-responsive" />
             </div>
 
             <div class={classes.gallery_product + " gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-12 filter spray" }>
-                <img src="http://fakeimg.pl/365x365/" class="img-responsive" />
+                <img src="/images/DSC_1661.jpg" class="img-responsive" />
             </div>
 
             <div class={classes.gallery_product + " gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-12 filter irrigation" }>
-                <img src="http://fakeimg.pl/365x365/" class="img-responsive" />
+                <img src="/images/DSC_1551.jpg" class="img-responsive" />
             </div>
 
             <div class={classes.gallery_product + " gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-12 filter irrigation" }>
-                <img src="http://fakeimg.pl/365x365/" class="img-responsive" />
+                <img src="/images/DSC_1600.jpg" class="img-responsive" />
             </div>
 
             <div class={classes.gallery_product + " gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-12 filter hdpe" }>
-                <img src="http://fakeimg.pl/365x365/" class="img-responsive" />
+                <img src="/images/DSC_1602.jpg" class="img-responsive" />
             </div>
-
-            <div class={classes.gallery_product +" gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-12 filter spray" }>
-                <img src="http://fakeimg.pl/365x365/" class="img-responsive" />
+            <div class={classes.gallery_product + " gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-12 filter sprinkle" }>
+                 <img src="/images/DSC_1633.jpg" class="img-responsive" />
             </div>
 
             <div class={classes.gallery_product + " gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-12 filter sprinkle" }>
-                 <img src="http://fakeimg.pl/365x365/" class="img-responsive" />
+                 <img src="/images/DSC_1567.jpg" class="img-responsive" />
             </div>
+            {/* <div class={classes.gallery_product +" gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-12 filter spray" }>
+                <img src="/images/DSC_1555.jpeg" class="img-responsive" />
+            </div> */}
+
+            {/*
+            <div class={classes.gallery_product + " gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-12 filter sprinkle" }>
+                 <img src="/images/DSC_1633.jpg" class="img-responsive" />
+            </div>
+            <div class={classes.gallery_product + " gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-12 filter sprinkle" }>
+                 <img src="/images/DSC_1633.jpg" class="img-responsive" />
+            </div> */}
         </div>
     </div>
 </section>
